042932e371d1e2c5b83545d1d08ed1a2bb3aa2f9
huili749 <156971456+huili749@users.noreply.github.com>
2025-01-23 19:44:09 -0500

    Update video

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -42,7 +42,7 @@ const Projects = ({ projects }) => {
                                     autoPlay
                                     loop
                                     muted
-                                    
+                                    playsInline
                                 ></video>
                             ) : (
                                 <img
